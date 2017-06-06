485aabe9018a41d5aaf4a31b040db66b27a0230f
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-06 09:26:11 -0600

    add def initialize and def stock_check to class Pantry + tests

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(ingredient)
+    0
+  end
+
+
+
+
+
 
 end
+
+#
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,23 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_stock_can_check_ingredients
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+
 end
